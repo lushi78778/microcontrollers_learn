deb35a957ff9a2575ab8b45236bdf95f7ee0da91
lushi <lushi78778@126.com>
2021-04-27 21:43:02 +0800

    add 7

--- 6/6.asm
+++ 6/6.asm
@@ -1,7 +1,8 @@
+;外部中断实验
 		LED BIT P1.0	  ;给P1.0起名字（连的发光二接管）
 		ORG 0000H		  ;定义程序的起始地址
 		LJMP START		  ;做跳转到0003H
-		ORG 0003H		  ;终端入口地址，中断 开始地址
+		ORG 0003H		  ;终断入口地址，中断 开始地址
 		LJMP INTERRUPT	  ;跳转到真正的终断地址
 		ORG 0030H		  ;主程序
 INTERRUPT: PUSH PSW		  ;中断，PSW入栈{保护PSW（程序状态字）}
--- 7/7.asm
+++ 7/7.asm
@@ -1,10 +1,11 @@
-		   ORG 0000H
-		   LJMP START
-		   ORG 0030H
-START:     MOV TMOD,#50H;0101,0000
-           MOV TH1,#0
-		   MOV TL1,#0
-		   SETB TR1
-	LOOP:  MOV P1,TL1
-	       AJMP LOOP
-		   END
+;计数器实验
+		   ORG 0000H		;定义程序的起始地址
+		   LJMP START		;做跳转到0003H
+		   ORG 0030H		;开始地址
+START:     MOV TMOD,#50H	;让定时计数器T1工作在计数方式 0101，0001
+           MOV TH1,#0		;TH1从零开始计数
+		   MOV TL1,#0		;TL1从零开始计数
+		   SETB TR1			;打开定时计数器
+	LOOP:  MOV P1,TL1		;等待中断计数, (TL1 0~255 进位是给TH1)
+	       AJMP LOOP		;一直循环
+		   END					   `
